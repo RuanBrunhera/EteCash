c4f0d656025b407c664d0949f5759bd0cc9e1422
lfdelima3 <lfdelimaa@gmail.com>
2026-01-14 11:04:24 -0300

    feat: creating routes for router.go

--- backend/router/router.go
+++ backend/router/router.go
@@ -4,11 +4,6 @@ import "github.com/gin-gonic/gin"
 
 func Init() {
 	r := gin.Default()
-	r.GET("/ping", func(ctx *gin.Context) {
-		ctx.JSON(200, gin.H{
-			"message": "Pong",
-		})
-	})
 
 	r.Run(":8080")
 }
