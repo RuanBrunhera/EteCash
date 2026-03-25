e556a4ad427c3c62914fa19d00b193501cee649c
Luis Fernando Antunes de Lima <lfdelimaa@gmail.com>
2026-03-25 12:13:24 +0000

    feito os outros models

--- backend/model/funcionarioModel.go
+++ backend/model/funcionarioModel.go
@@ -1,1 +1,10 @@
 package model
+
+import "time"
+
+type Funcionario struct {
+	ID       uint       `json:"id"         gorm:"primaryKey"`
+	Nome     string     `json:"nome"       gorm:"not null;column:nome"      validate:"required,min=3,max=100"`
+	DataNasc *time.Time `json:"data_nasc"  gorm:"column:data_nasc"`
+	Telefone *string    `json:"telefone"   gorm:"column:telefone"            validate:"omitempty,min=10,max=20"`
+}
--- backend/model/transacaoModel.go
+++ backend/model/transacaoModel.go
@@ -1,1 +1,31 @@
 package model
+
+import "time"
+
+type Transacao struct {
+	ID            uint      `json:"id"             gorm:"primaryKey"`
+	AlunoRM       int       `json:"aluno_rm"       gorm:"not null;column:aluno_rm"       validate:"required"`
+	FuncionarioID uint      `json:"funcionario_id" gorm:"not null;column:funcionario_id"  validate:"required"`
+	ValorTotal    float64   `json:"valor_total"    gorm:"type:numeric(10,2);not null"     validate:"required,gt=0"`
+	DataHora      time.Time `json:"data_hora"      gorm:"column:data_hora;default:CURRENT_TIMESTAMP"`
+
+	// Relacionamentos
+	Aluno       Aluno           `json:"aluno"          gorm:"foreignKey:AlunoRM;references:RM"`
+	Funcionario Funcionario     `json:"funcionario"    gorm:"foreignKey:FuncionarioID"`
+	Itens       []ItemTransacao `json:"itens"          gorm:"foreignKey:TransacaoID"`
+}
+
+type ItemTransacao struct {
+	ID            uint    `json:"id"             gorm:"primaryKey"`
+	TransacaoID   uint    `json:"transacao_id"   gorm:"not null;column:transacao_id"  validate:"required"`
+	ProdutoID     uint    `json:"produto_id"     gorm:"not null;column:produto_id"    validate:"required"`
+	Quantidade    int     `json:"quantidade"     gorm:"not null;check:quantidade > 0" validate:"required,min=1"`
+	PrecoUnitario float64 `json:"preco_unitario" gorm:"type:numeric(10,2);not null"   validate:"required,gt=0"`
+
+	// Relacionamento
+	Produto Produto `json:"produto"        gorm:"foreignKey:ProdutoID"`
+}
+
+func (ItemTransacao) TableName() string {
+	return "item_transacao"
+}
