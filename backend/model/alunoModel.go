16a46a94dcad233a513f58e5ba48d7f7e4d6dc2e
Luis Fernando Antunes de Lima <lfdelimaa@gmail.com>
2026-03-25 12:03:06 +0000

    feito um poquinho do alunoModel

--- backend/model/alunoModel.go
+++ backend/model/alunoModel.go
@@ -1,1 +1,8 @@
 package model
+
+type Aluno struct {
+	RM    int     `json:"rm"     gorm:"primaryKey;column:rm"`
+	Nome  string  `json:"nome"   gorm:"not null;column:nome"          validate:"required,min=3,max=100"`
+	Serie string  `json:"serie"  gorm:"type:char(1);not null;column:serie" validate:"required,oneof=1 2 3"`
+	Saldo float64 `json:"saldo"  gorm:"type:numeric(10,2);default:0"   validate:"min=0"`
+}
