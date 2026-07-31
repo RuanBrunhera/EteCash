f388d1db6a0b12c60fcbeaa007e461c4ae7ff32f
Ruan246Etec <ruan.projeto3@gmail.com>
2026-07-31 23:21:36 +0000

    db: atualiza schema e seed de alunos

--- database/seed/aluno.sql
+++ database/seed/aluno.sql
@@ -1,11 +1,12 @@
-INSERT INTO aluno (rm, nome, serie, curso_id, saldo, senha) 
+INSERT INTO aluno (rm, nome, serie, curso_id, saldo, senha, pin) 
 VALUES (
     123456,
     'João Silva',
     '1',
     1,
     0.00,
-    'ef92b778bafe771e89245b89ecbc08a44a4e166c06659911881f383d4473e94f' -- senha: "password"
+    'ef92b778bafe771e89245b89ecbc08a44a4e166c06659911881f383d4473e94f', -- senha: "password"
+    '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4' -- pin: "1234"
 ),
 (
     20242460054,
@@ -13,5 +14,6 @@ VALUES (
     '3',
     1,
     0.00,
-    '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8' -- senha: "password"
+    '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', -- senha: "password"
+    '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4' -- pin: "1234"
 );
