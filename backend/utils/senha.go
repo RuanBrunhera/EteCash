bb637167de1e69d8c1da8a6b0fe6ba99aa62954f
Ruan246Etec <ruan.projeto3@gmail.com>
2026-08-26 22:53:55 +0000

    feat: adiciona geração de senha temporária alfanumérica

---
+++ backend/utils/senha.go
@@ -0,0 +1,15 @@
+package utils
+
+import (
+	"math/rand"
+)
+
+func GerarSenhaTemporaria() string {
+	// Versão alfanumérica
+	const caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+	senha := make([]byte, 8)
+	for i := range senha {
+		senha[i] = caracteres[rand.Intn(len(caracteres))]
+	}
+	return string(senha)
+}
